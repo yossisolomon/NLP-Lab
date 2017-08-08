347fa041bf34b04c48b76daa3cddd6229d4190a0
Yossi Solomon <yosisolomon@gmail.com>
2017-08-09 01:25:39 +0300

    Updated version: +load lines + function words - multi-file support dropped + word chunking + change script name appropriately

--- speakers_native_lang_detection.py
+++ speakers_native_lang_detection.py
@@ -5,11 +5,15 @@ import logging
 import xml.etree.ElementTree as etree
 import json
 import matplotlib.pyplot as plt
+from random import shuffle
+from nltk import FreqDist
+from en_function_words import FUNCTION_WORDS
 
 ID_SUFFIX = ".id"
 EN_SUFFIX = ".en.tok"
 FR_SUFFIX = ".fr.tok"
 EN_LINES_KEY = "EN_LINES"
+EN_NON_NATIVE_LINES_KEY = "NON_NATIVE_LINES"
 FR_LINES_KEY = "FR_LINES"
 EN_PERCENT_KEY = "EN_PERCENT"
 FR_PERCENT_KEY = "FR_PERCENT"
@@ -34,7 +38,8 @@ def separate_identification(line):
     return line_count, name, lang
 
 
-def add_speakers_lines_from_id_file(filename,speakers):
+def add_speakers_lines_from_id_file(filename):
+    speakers = {}
     logging.info("Running speaker extraction from "+filename)
     with open(filename) as f:
         for line in f:
@@ -49,19 +54,35 @@ def add_speakers_lines_from_id_file(filename,speakers):
 
 
 def output_speakers_json(output_location, speakers):
-    output = output_location
+    output = output_location + "speakers-out.json"
     logging.info("Outputting speakers to " + output)
     with open(output, 'w') as out:
         json.dump(speakers, out, sort_keys=True, indent=4, separators=(',', ': '))
 
 
+def output_lines_json(output_location, lines):
+    output = output_location + "lines-out.json"
+    logging.info("Outputting lines to " + output)
+    with open(output, 'w') as out:
+        json.dump(lines, out, indent=4, separators=(',', ': '))
+
+
 def load_speakers_json(output_location):
-    file = output_location
+    file = output_location + "speakers-out.json"
     logging.info("Loading speakers from " + file)
     speakers = {}
     with open(file) as f:
         speakers = json.load(f)
     return speakers
+
+
+def load_lines_json(output_location):
+    file = output_location + "lines-out.json"
+    logging.info("Loading lines from " + file)
+    lines = {}
+    with open(file) as f:
+        lines = json.load(f)
+    return lines
 
 
 def add_speakers_stats(speakers):
@@ -88,11 +109,8 @@ def create_minimum_lines_speakers_graph(speakers):
     plt.show()
 
 
-def get_speakers_stats_from_id_file(file_patterns):
-    speakers = {}
-    for pattern in file_patterns:
-        logging.info("Running speaker extraction from pattern " + pattern)
-        add_speakers_lines_from_id_file(pattern + ID_SUFFIX, speakers)
+def get_speakers_stats_from_id_file(file_pattern):
+    speakers = add_speakers_lines_from_id_file(file_pattern + ID_SUFFIX)
     logging.info("Found " + str(len(speakers)) + " total speakers")
     add_speakers_stats(speakers)
     return speakers
@@ -118,11 +136,13 @@ def separate_lines_by_lang(speakers, separator):
 def parse_args():
     p = argparse.ArgumentParser()
     p.add_argument("-d","--debug",action='store_true')
-    p.add_argument("-p","--file-patterns", nargs='+', required=True, help="The base file name pattern where the files with suffixes {%s,%s,%s} are"%(ID_SUFFIX, EN_SUFFIX, FR_SUFFIX))
-    p.add_argument("--output-location",default="/tmp/speakers-out.json")
+    p.add_argument("-p","--file-pattern", required=True, help="The base file name pattern where the files with suffixes {%s,%s,%s} are"%(ID_SUFFIX, EN_SUFFIX, FR_SUFFIX))
+    p.add_argument("--output-location",default="/tmp/")
     p.add_argument("--speaker-stats-from-file",action='store_true')
+    p.add_argument("--lines-from-file",action='store_true', help="Bypasses the speaker separation part")
     p.add_argument("--show-graph",action='store_true')
-    p.add_argument("-t","--threshold",type=float,default=0.1)
+    p.add_argument("-t","--threshold",type=float,default=0.5)
+    p.add_argument("-c","--chunk-size",type=int,default=500)
     return p.parse_args()
 
 
@@ -138,29 +158,96 @@ def assert_no_duplicates(checked_list):
     full_len = len(checked_list)
     set_len = len(set(checked_list))
     msg = "full="+str(full_len)+" set="+str(set_len)
-    assert (full_len - set_len == 0, msg)
+    assert full_len - set_len == 0, msg
+
+
+def lines_to_word_chunks(class_lines, chunk_size, to_shuffle=False):
+    curr_chunk = []
+    chunks = []
+    if to_shuffle:
+        shuffle(class_lines)
+    for l in class_lines:
+        split_line = l.split()
+        curr_chunk.extend(split_line)
+        if len(curr_chunk) >= chunk_size:
+            chunks.append(curr_chunk)
+            curr_chunk = []
+    return chunks
+
+
+def get_func_word_freq(words, function_words):
+    freq_dist = FreqDist()
+    for w in words:
+        if w in function_words:
+            freq_dist[w] += 1
+    fw_freq = {}
+    for key,value in freq_dist.iteritems():
+        fw_freq[key] = value
+    return fw_freq
+
+
+def extract_lines_from_corpus_by_index(line_nums, lines_file):
+    lines = map(lambda i: lines_file[i], line_nums)
+    assert len(lines) == len(line_nums)
+    return lines
+
+
+def check_class_list(class_list):
+    assert_no_duplicates(class_list)
+    assert len(class_list) > 0, "Class list is empty..."
 
 
 if __name__ == '__main__':
     args = parse_args()
     set_logging(args.debug)
 
-    speakers = {}
-    if args.speaker_stats_from_file:
-        speakers = load_speakers_json(args.output_location)
+    lines = {}
+    if args.lines_from_file:
+        lines = load_lines_json(args.output_location)
     else:
-        speakers = get_speakers_stats_from_id_file(args.file_patterns)
-        output_speakers_json(args.output_location, speakers)
+        speakers = {}
+        if args.speaker_stats_from_file:
+            speakers = load_speakers_json(args.output_location)
+        else:
+            speakers = get_speakers_stats_from_id_file(args.file_pattern)
+            output_speakers_json(args.output_location, speakers)
 
-    if args.show_graph:
-        create_minimum_lines_speakers_graph(speakers)
+        if args.show_graph:
+            create_minimum_lines_speakers_graph(speakers)
 
-    separator = NativeSpeakerSeparator(args.threshold)
-    en_native_lines, en_non_native_lines, fr_native_lines = separate_lines_by_lang(speakers,separator)
-    logging.info("Checking for line duplicates...")
-    assert_no_duplicates(en_native_lines)
-    assert_no_duplicates(en_non_native_lines)
-    assert_no_duplicates(fr_native_lines)
-    logging.info("All Good!")
+        separator = NativeSpeakerSeparator(args.threshold)
+        en_native_line_nums, en_non_native_line_nums, fr_native_line_nums = separate_lines_by_lang(speakers, separator)
 
-    #TODO Add NLP part...
+        en_native_line_nums = map(lambda i: int(i), en_native_line_nums)
+        en_non_native_line_nums = map(lambda i: int(i), en_non_native_line_nums)
+        fr_native_line_nums = map(lambda i: int(i), fr_native_line_nums)
+
+        logging.info("Checking for line class list problems...")
+        check_class_list(en_native_line_nums)
+        check_class_list(en_non_native_line_nums)
+        check_class_list(fr_native_line_nums)
+        logging.info("All Good!")
+
+        lines[EN_LINES_KEY] = []
+        lines[EN_NON_NATIVE_LINES_KEY] = []
+        lines[FR_LINES_KEY] = []
+        f = args.file_pattern + EN_SUFFIX
+        logging.info("Extracting lines from " + f)
+        with open(f) as lines_file:
+            i = 0
+            for l in lines_file:
+                i += 1
+                if i in en_native_line_nums:
+                    lines[EN_LINES_KEY].append(l)
+                elif i in en_non_native_line_nums:
+                    lines[EN_NON_NATIVE_LINES_KEY].append(l)
+                elif i in fr_native_line_nums:
+                    lines[FR_LINES_KEY].append(l)
+                else:
+                    logging.debug("Line " + str(i) + " in the corpus is not relevant (probably french line by english native speaker)")
+
+        output_lines_json(args.output_location, lines)
+
+    en_native_chunks = lines_to_word_chunks(lines[EN_LINES_KEY], args.chunk_size)
+    en_non_native_chunks = lines_to_word_chunks(lines[EN_NON_NATIVE_LINES_KEY], args.chunk_size)
+    fr_native_chunks = lines_to_word_chunks(lines[FR_LINES_KEY], args.chunk_size)
