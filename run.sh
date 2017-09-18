8b55dde32da1acabaaf8eee2c65a89fc47b3c0cc
Yossi Solomon <yosisolomon@gmail.com>
2017-09-19 01:55:00 +0300

    Rewrite run script

--- run.sh
+++ run.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
-for i in 0.1 0.2 0.3 0.4 0.5 ; do
-    python corpus_to_chunks.py -p hansard.EN-FR/hansard/train --speaker-stats-from-file -t $i && python native_lang_detection.py ;
+
+
+file_pattern="hansard.EN-FR/committees/train"
+
+echo Running Native, Non-Native, Translated NLP analysis
+
+echo We are using the file pattern $file_pattern
+
+echo
+
+while true; do
+    read -p $'Please make sure to close figure windows after looking/saving to allow continuation of the analysis, OK? [Yy/Nn] \n' yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes [Y/y] or no [N/n].";;
+    esac
 done
+
+set -x
+
+python corpus_to_files.py -p $file_pattern --show-graph
+python calc_corpus_metrics.py --show-graph
+python calc_chunk_counts.py --pos-counts --function-word-counts
+python native_lang_detection.py
